d3b2741fb105c34304d575bfac552deb709577ee
Shoma Kobayashi <sho1228.k@gmail.com>
2022-03-21 10:45:23 +0900

    [p18] `return`などのキーワードも定義できることを確認

--- token/token.go
+++ token/token.go
@@ -36,11 +36,21 @@ const (
 
 	FUNCTION = "FUNCTION"
 	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
 )
 
 var keywords = map[string]TokenType{
 	"fn": FUNCTION,
 	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
 }
 
 // keyword テーブルを確認して、渡された識別子がキーワードかを確認する
