c950caced053b2fadcb360ce78aaa70ffe808326
Shoma Kobayashi <sho1228.k@gmail.com>
2022-03-21 09:43:09 +0900

    [p9] テストが通ることを確認

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/shoma3571/go_interpreter/token"
+
 type lexer struct {
 	input string
 	position int // 入力における現在の位置(現在の位置を指し示す)
@@ -10,6 +12,7 @@ type lexer struct {
 func New(input string) *lexer {
 	// inputのみを定義して、他はゼロ値で設定
 	l := &lexer{input: input}
+	// とりあえず最初の文字を読んでおく
 	l.readChar()
 	return l
 }
@@ -27,4 +30,41 @@ func (l *lexer) readChar() {
 	// 位置の更新
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+
+// 現在検査中の文字 l.ch を見て、それに応じてトークンを返す
+// 返す前に入力のポインタを進めて、次に読んだ時に位置が更新されるようにする
+func (l *lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+// token初期化の役割を果たす
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
