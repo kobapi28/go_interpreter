c8161b0a77a66cc0f9d7e308fdcc6c9c8a19ec7c
Shoma Kobayashi <sho1228.k@gmail.com>
2022-03-25 11:30:53 +0900

    [p145] Returnを評価するための`ReturnValue`を作成

--- object/object.go
+++ object/object.go
@@ -10,6 +10,7 @@ const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
 	NULL_OBJ    = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 type Object interface {
@@ -46,4 +47,15 @@ func (n *Null) Inspect() string {
 }
 func (n *Null) Type() ObjectType {
 	return NULL_OBJ
+}
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType {
+	return RETURN_VALUE_OBJ
+}
+func (rv *ReturnValue) Inspect() string {
+	return rv.Value.Inspect()
 }
