94ea1bd8ff9bc325ae62f38ac92ce5a652439814
Shoma Kobayashi <sho1228.k@gmail.com>
2022-03-25 21:14:42 +0900

    refactor: `Eval`を`eval.go`に切り出し、`evaluator.go`を`evaluators.go`にリネーム

---
+++ evaluator/eval.go
@@ -0,0 +1,70 @@
+package evaluator
+
+import (
+	"github.com/shoma3571/go_interpreter/ast"
+	"github.com/shoma3571/go_interpreter/object"
+)
+
+func Eval(node ast.Node, env *object.Environment) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalProgram(node, env)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression, env)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right, env)
+		if isError(right) {
+			return right
+		}
+		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+		right := Eval(node.Right, env)
+		if isError(right) {
+			return right
+		}
+		return evalInfixExpression(node.Operator, left, right)
+	case *ast.BlockStatement:
+		return evalBlockStatement(node, env)
+	case *ast.IfExpression:
+		return evalIfExpression(node, env)
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue, env)
+		if isError(val) {
+			return val
+		}
+		return &object.ReturnValue{Value: val}
+	case *ast.LetStatement:
+		val := Eval(node.Value, env)
+		if isError(val) {
+			return val
+		}
+		env.Set(node.Name.Value, val)
+	case *ast.Identifier:
+		return evalIdentifier(node, env)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+
+		return applyFunction(function, args)
+	}
+
+	return nil
+}
